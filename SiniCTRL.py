b8135bc28f0a64b922f3800ce433c9096211bbc9
cdntinker <cdntinker@gmail.com>
2023-06-07 03:08:37 -0400

    Turnin' hard codeed stuff soft coded...

--- SiniCTRL.py
+++ SiniCTRL.py
@@ -24,10 +24,16 @@ import time
 # These are MY SiniLink USB switches
 #--------------------------------------
 mqttBroker ="Skynet"
-Device0 = "stat/Sinilink_0/Power"
-Device1 = "stat/Sinilink_1/Power00"
-Device2 = "stat/Sinilink_2/Power"
-Device3 = "stat/Sinilink_3/Power"
+WindowTitle = "SiniLink CTRLs"
+Device0 = "SiniLink_0"
+Device1 = "SiniLink_1"
+Device2 = "SiniLink_2"
+Device3 = "SiniLink_3"
+
+State_0 = "wtf"
+State_1 = "wtf"
+State_2 = "wtf"
+State_3 = "wtf"
 
 client_id = f'SiniLink_Controls-{time.time()}'
 # This needs to be randomized if you might run more than one instance...
@@ -44,47 +50,44 @@ def on_connect(client, userdata, flags, rc):
         print(client_id, "is connected to", mqttBroker)
         #--------------------------------------
         client.subscribe([
-            (Device0,0),
-            (Device1,0),
-            (Device2,0),
-            (Device3,0),
+            ("stat/"+Device0+"/Power",0),
+            ("stat/"+Device1+"/Power00",0),
+            ("stat/"+Device2+"/Power",0),
+            ("stat/"+Device3+"/Power",0),
             ])
-        print('Subscribed to:\n\t{:s}\n\t{:s}\n\t{:s}\n\t{:s}'.format(Device0, Device1, Device2, Device3))
+        print('Subscribed to:\n\t{:s}\n\t{:s}\n\t{:s}\n\t{:s}'.format(
+            Device0, 
+            Device1, 
+            Device2, 
+            Device3
+            ))
         #--------------------------------------
-
     else:
         print("Bad connection Returned code=",rc)
-
-client = mqtt.Client(client_id)
-client.will_set(lwt_topic, payload="(I B ded)")
-
-client.on_connect=on_connect  #bind call back function
-
-# client.connect(mqttBroker) 
 
 def on_message(client, userdata, message):
     #--------------------------------------
     # Yet ANOTHER truly FUGLY part...
     #--------------------------------------
-    global State_0, State_1, State_2, State_3
+    # global State_0, State_1, State_2, State_3
 
     print("  topic:", message.topic)
     print("message:", str(message.payload.decode("utf-8")))
 
-    if(message.topic == "stat/Sinilink_0/Power"):
+    if(message.topic == "stat/"+Device0+"/Power"):
         State_0 = str(message.payload.decode("utf-8"))
-        print("MoFo! 0 " + State_0)
-    if(message.topic == "stat/Sinilink_1/Power00"):
+    if(message.topic == "stat/"+Device1+"/Power00"):        # This one is weird because the firmware is a little weird...
         State_1 = str(message.payload.decode("utf-8"))
-        print("MoFo! 1 " + State_1)
-    if(message.topic == "stat/Sinilink_2/Power"):
+    if(message.topic == "stat/:+Device2+/Power"):
         State_2 = str(message.payload.decode("utf-8"))
-        print("MoFo! 2 " + State_2)
-    if(message.topic == "stat/Sinilink_3/Power"):
+    if(message.topic == "stat/"+Device3+"/Power"):
         State_3 = str(message.payload.decode("utf-8"))
-        print("MoFo! 3 " + State_3)
     #--------------------------------------
 
+client = mqtt.Client(client_id)
+client.will_set(lwt_topic, payload="(I B ded)")
+
+client.on_connect=on_connect  #bind call back function
 client.on_message=on_message        #attach function to callback
 
 #------------------------------------------------------------------------------
@@ -98,6 +101,7 @@ class TOGGLE_WINDOW:
         return False
 
     def destroy(self, widget, data=None):
+        print('destroy event occurred')
         Gtk.main_quit()
 
     #--------------------------------------
@@ -106,7 +110,7 @@ class TOGGLE_WINDOW:
     def __init__(self):
         TheWindow = Gtk.Window()
         TheWindow.set_position(Gtk.WindowPosition.CENTER)
-        TheWindow.set_title('SiniLink CTRLs')
+        TheWindow.set_title(WindowTitle)
         TheWindow.connect('delete_event', self.delete_event)
         TheWindow.connect('destroy', self.destroy)
 
@@ -116,19 +120,19 @@ class TOGGLE_WINDOW:
         # a loop.  Maybe build up a set of
         # variables to define the devices
         #--------------------------------------
-        self.toggle0 = Gtk.ToggleButton(label = 'SiniLink 0 - wtf')
+        self.toggle0 = Gtk.ToggleButton(label = Device0+' - wtf')
         self.toggle0.connect('toggled', self.on_toggled0, 'toggle')
         self.toggle0.set_size_request(200, 0)
 
-        self.toggle1 = Gtk.ToggleButton(label = 'SiniLink 1 - wtf')
+        self.toggle1 = Gtk.ToggleButton(label = Device1+' - wtf')
         self.toggle1.connect('toggled', self.on_toggled1, 'toggle')
         self.toggle0.set_size_request(300, 0)
 
-        self.toggle2 = Gtk.ToggleButton(label = 'SiniLink 2 - wtf')
+        self.toggle2 = Gtk.ToggleButton(label = Device2+' - wtf')
         self.toggle2.connect('toggled', self.on_toggled2, 'toggle')
         self.toggle0.set_size_request(200, 0)
 
-        self.toggle3 = Gtk.ToggleButton(label = 'SiniLink 3 - wtf')
+        self.toggle3 = Gtk.ToggleButton(label = Device3+' - wtf')
         self.toggle3.connect('toggled', self.on_toggled3, 'toggle')
         self.toggle0.set_size_request(200, 0)
         #--------------------------------------
@@ -157,46 +161,44 @@ class TOGGLE_WINDOW:
 
         if state == True:
             self.label = self.toggle0.get_child()
-            self.label.set_markup('<b>SiniLink 0 - ON </b>')  
-            client.publish("cmnd/Sinilink_0/Power", "on")
+            self.label.set_markup('<b>'+Device0+' - ON </b>')  
+            client.publish("cmnd/"+Device0+"/Power", "on")
         else:
-            self.toggle0.set_label('SiniLink 0 - OFF')
-            client.publish("cmnd/Sinilink_0/Power", "off")
+            self.toggle0.set_label(''+Device0+' - OFF')
+            client.publish("cmnd/"+Device0+"/Power", "off")
 
     def on_toggled1(self, event, widget):
         state = self.toggle1.get_active()
 
         if state == True:
             self.label = self.toggle1.get_child()
-            self.label.set_markup('<b>SiniLink 1 - ON </b>')  
-            client.publish("cmnd/Sinilink_1/Power", "on")
-            print("Sent a ON command via MQTT to Sinilink_1")
+            self.label.set_markup('<b>'+Device1+' - ON </b>')  
+            client.publish("cmnd/"+Device1+"/Power", "on")
         else:
-            self.toggle1.set_label('SiniLink 1 - OFF')
-            client.publish("cmnd/Sinilink_1/Power", "off")
-            print("Sent a OFF command via MQTT to Sinilink_1")
+            self.toggle1.set_label(''+Device1+' - OFF')
+            client.publish("cmnd/"+Device1+"/Power", "off")
 
     def on_toggled2(self, event, widget):
         state = self.toggle2.get_active()
 
         if state == True:
             self.label = self.toggle2.get_child()
-            self.label.set_markup('<b>SiniLink 2 - ON </b>')  
-            client.publish("cmnd/Sinilink_2/Power", "on")
+            self.label.set_markup('<b>'+Device2+' - ON </b>')  
+            client.publish("cmnd/"+Device2+"/Power", "on")
         else:
-            self.toggle2.set_label('SiniLink 2 - OFF')
-            client.publish("cmnd/Sinilink_2/Power", "off")
+            self.toggle2.set_label(''+Device2+' - OFF')
+            client.publish("cmnd/"+Device2+"/Power", "off")
 
     def on_toggled3(self, event, widget):
         state = self.toggle3.get_active()
 
         if state == True:
             self.label = self.toggle3.get_child()
-            self.label.set_markup('<b>SiniLink 3 - ON </b>')  
-            client.publish("cmnd/Sinilink_3/Power", "on")
+            self.label.set_markup('<b>'+Device3+' - ON </b>')  
+            client.publish("cmnd/"+Device3+"/Power", "on")
         else:
-            self.toggle3.set_label('SiniLink 3 - OFF')
-            client.publish("cmnd/Sinilink_3/Power", "off")
+            self.toggle3.set_label(''+Device3+' - OFF')
+            client.publish("cmnd/"+Device3+"/Power", "off")
     #--------------------------------------
 
     def main(self):
